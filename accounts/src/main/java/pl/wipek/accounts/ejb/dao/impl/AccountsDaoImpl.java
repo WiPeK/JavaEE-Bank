d98a043e3235e5854b08c6910a99ef33fa43972e
WiPeK <wipekxxx@gmail.com>
2017-12-18 21:37:19 +0100

    Merged orm to master

--- accounts/src/main/java/pl/wipek/accounts/ejb/dao/impl/AccountsDaoImpl.java
+++ accounts/src/main/java/pl/wipek/accounts/ejb/dao/impl/AccountsDaoImpl.java
@@ -24,38 +24,4 @@ public class AccountsDaoImpl extends AbstractDao<String, Account> implements Acc
         System.out.println(query);
         return new HashSet<>(query.getResultList());
     }
-
-    private Set<Account> getMockedAccounts() {
-        Set<Account> accounts = new HashSet<>();
-//
-//        Account account1 = new Account();
-//        account1.setAccountNumber("PL831050171937392025842606809009");
-//        account1.setBalance(new BigDecimal(1234.56));
-//        account1.setCurrency("PLN");
-//        account1.setName("NORMAL");
-//
-//        Account account2 = new Account();
-//        account2.setAccountNumber("EU709195080496737562156108531824");
-//        account2.setBalance(new BigDecimal(1234.56));
-//        account2.setCurrency("EUR");
-//        account2.setName("VIP");
-//
-//        Account account3 = new Account();
-//        account3.setAccountNumber("US560958676315792527370876707478");
-//        account3.setBalance(new BigDecimal(1234.56));
-//        account3.setCurrency("USD");
-//        account3.setName("<20");
-//
-//        Account account4 = new Account();
-//        account4.setAccountNumber("PL129253072017537338784286639998");
-//        account4.setBalance(new BigDecimal(1234.56));
-//        account4.setCurrency("PLN");
-//        account4.setName("NORMAL");
-//
-//        accounts.add(account1);
-//        accounts.add(account2);
-//        accounts.add(account3);
-//        accounts.add(account4);
-        return accounts;
-    }
 }
